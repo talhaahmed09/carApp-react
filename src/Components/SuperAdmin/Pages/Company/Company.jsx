683b5d9555c4f72cc6344609f43f1ab2a8185354
umair <umair.ahmad1257@gmail.com>
2023-02-16 20:39:09 +0500

    fix: pagination is now working pagination is set up for server-side pagination. bug: data is not updating

--- src/Components/SuperAdmin/Pages/Company/Company.jsx
+++ src/Components/SuperAdmin/Pages/Company/Company.jsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { useState } from "react";
-import { Toolbar } from "@mui/material";
+import { TablePagination, Toolbar } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
+import { LastPage, FirstPage, KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
+import {Box, IconButton} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -13,18 +15,69 @@ import Paper from "@mui/material/Paper";
 import { CreateBtn } from "../../../Buttons";
 import { Createcompany } from "./Createcompany";
 import AuthUser from "../../Auth/AuthUser";
-import {Pageloader} from "../Page loader/Pageloader";
+import { Pageloader } from "../Page loader/Pageloader";
 import usePagination from "../Pagination/Pagination";
 import SelectPopover from "../SelectPopover";
 
+function TablePaginationActions(props) {
+  const { count, page, rowsPerPage, onPageChange } = props;
 
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5, mb: "1rem" }} >
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        <FirstPage />
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        <KeyboardArrowLeft />
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        <KeyboardArrowRight />
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+         <LastPage />
+      </IconButton>
+    </Box>
+  );
+}
 
 export function Company() {
   const { http } = AuthUser();
   const [companylist, setCompanylist] = useState([]);
   const [editIndex, setEditIndex] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [editItem, setEditItem] = useState()
+  const [editItem, setEditItem] = useState();
 
   React.useEffect(() => {
     fetchListCompany();
@@ -40,115 +93,116 @@ export function Company() {
     setLoading(false);
   };
 
- 
-
   // pagination
-  let [page, setPage] = useState(1);
+  let [page, setPage] = useState(0);
   const PER_PAGE = 5;
+  const [rowsPerPage, setRowsPerPage] = useState(PER_PAGE);
 
-  const count = Math.ceil(companylist.length / PER_PAGE);
-  const _DATA = usePagination(companylist, PER_PAGE);
+  const count = companylist.length
+  const _DATA = usePagination(companylist, rowsPerPage);
 
-
-  const paginationHandler = (e, p) => {
+  const paginationHandler = (e,p) => {
     setPage(p);
     _DATA.jump(p);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   return (
     <>
       {compCheck || editIndex != null ? (
-        <Createcompany editIndex={editIndex}  editItem = {editItem} />
+        <Createcompany editIndex={editIndex} editItem={editItem} />
       ) : (
         <>
           <div style={{ height: 400, width: "100%" }}>
             <Toolbar />
-          
-              <div className="flex justify-between">
-                <h1 className="text-base text-bold mb-0 ml-5">
-                  List of Company
-                </h1>
-                <div className="mr-5">
-                  <CreateBtn
-                    name="Create"
-                    icon={<AddIcon />}
-                    onClick={() => setCompCheck(!compCheck)}
-                  />
-                </div>
+
+            <div className="flex justify-between">
+              <h1 className="text-base text-bold mb-0 ml-5">List of Company</h1>
+              <div className="mr-5">
+                <CreateBtn
+                  name="Create"
+                  icon={<AddIcon />}
+                  onClick={() => setCompCheck(!compCheck)}
+                />
               </div>
-            
+            </div>
 
             <TableContainer component={Paper}>
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
-               
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Title</TableCell>
-                      <TableCell align="center">Contact</TableCell>
-                      <TableCell align="center">Address</TableCell>
-                      <TableCell align="center">Action</TableCell>
-                    </TableRow>
-                  </TableHead>
-                
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Title</TableCell>
+                    <TableCell align="center">Contact</TableCell>
+                    <TableCell align="center">Address</TableCell>
+                    <TableCell align="center">Action</TableCell>
+                  </TableRow>
+                </TableHead>
+
                 {loading ? (
                   <Pageloader />
                 ) : (
                   <TableBody>
                     {companylist &&
-                    _DATA.currentData().map((data, index) => {
-                      return (
-                                <TableRow
-                                  key={data.id}
-                                  sx={{
-                                    "&:last-child td, &:last-child th": {
-                                      border: 0,
-                                    },
-                                  }}
-                                >
-                                  <TableCell component="th" scope="row">
-                                    <p className="mb-0">{data.name}</p>
-                                    <p className="mb-0 text-slate-400">
-                                      {data.email}
-                                    </p>
-                                  </TableCell>
-                                  <TableCell align="center">
-                                    {data.mobile}
-                                  </TableCell>
-                                  <TableCell align="center">
-                                    {data.city}
-                                  </TableCell>
-                                  <TableCell align="center">
-                                 
-                                    <SelectPopover
-                                    {...data}
-                                    apiName="company"
-                                    SetState={setCompanylist}
-                                    state={companylist}
-                                    setEditIndex={setEditIndex}
-                                    index={index}
-                                    setEditItem ={setEditItem}
-                                  />
-                                  
-                                  </TableCell>
-
-                                </TableRow>
-                             
-                      )
-                    })
-                  
-                  }
+                      _DATA.currentData().map((data, index) => {
+                        return (
+                          <TableRow
+                            key={data.id}
+                            sx={{
+                              "&:last-child td, &:last-child th": {
+                                border: 0,
+                              },
+                            }}
+                          >
+                            <TableCell component="th" scope="row">
+                              <p className="mb-0">{data.name}</p>
+                              <p className="mb-0 text-slate-400">
+                                {data.email}
+                              </p>
+                            </TableCell>
+                            <TableCell align="center">{data.mobile}</TableCell>
+                            <TableCell align="center">{data.city}</TableCell>
+                            <TableCell align="center">
+                              <SelectPopover
+                                {...data}
+                                apiName="company"
+                                SetState={setCompanylist}
+                                state={companylist}
+                                setEditIndex={setEditIndex}
+                                index={index}
+                                setEditItem={setEditItem}
+                              />
+                            </TableCell>
+                          </TableRow>
+                        );
+                      })}
                   </TableBody>
                 )}
               </Table>
             </TableContainer>
             {editIndex != null ? null : !loading ? (
               <div className="mt-3 flex justify-end">
-                <Pagination
+                <TablePagination
+                  count={count}
+                  variant="outlined"
+                  shape="rounded"
+                  onPageChange={paginationHandler}
+                  rowsPerPage={rowsPerPage}
+                  SelectProps={{sx: {mb: '1rem'}}}
+                  page={page}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  rowsPerPageOptions={[5, 10, 25]}
+                  ActionsComponent={TablePaginationActions}
+                />
+                {/* <Pagination
                   count={count}
                   variant="outlined"
                   shape="rounded"
                   onChange={paginationHandler}
-                />
+                /> */}
               </div>
             ) : null}
           </div>

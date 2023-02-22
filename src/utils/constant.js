4b37a902ae567b4029b7cf83bf4ac9d5b6959dc6
umair <umair.ahmad1257@gmail.com>
2023-02-22 21:55:13 +0500

    fix: brings file from bug-user-dev

---
+++ src/utils/constant.js
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  401: "Unable to Authorize at this time, Please try again later",
+  403: "User not Allowed, Please Contact Administrator",
+  404: "Data Not Found, Please try again later",
+  500: "Internal Server Error, Please try again later",
+};
